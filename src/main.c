c3f403643cb29e506d519002d6b781653d35eaaf
maherfauzi <azirariza9@gmail.com>
2025-03-02 12:39:29 +0700

    ADD read tasks from file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+void fRead(FILE *);
+
 int main(void) {
   // TO DO :
   // iterate over tasks and then write it to file
-  
+
   (void)printf("Is the file exist yet?, create file if not\n");
 
   FILE *pFile;
@@ -32,6 +34,8 @@ int main(void) {
 
   if (c != EOF) {
     (void)printf("File not Empty\n");
+    (void)ungetc(c, pFile);
+    (void)fRead(pFile);
     (void)printf("Closing file...\n");
     int closeStatus = fclose(pFile);
     if (closeStatus == EOF) {
@@ -41,9 +45,11 @@ int main(void) {
     return 0;
   }
 
+  (void)ungetc(c, pFile);
+
   (void)printf("Beyond here file empty, start write!!\n");
 
-  if (strcmp(openMode, "r") == 0) {
+  if (strcmp(openMode, "w") != 0) {
     (void)printf("Change open mode to write only\n");
     openMode = "w";
     pFile = freopen(fileName, openMode, pFile);
@@ -54,22 +60,53 @@ int main(void) {
     }
   }
 
-  for (size_t i = 0; i < (sizeof(tasks)/sizeof(tasks[0]) ); i++) {
+  for (size_t i = 0; i < (sizeof(tasks) / sizeof(tasks[0])); i++) {
 
     int writeFile = fputs(tasks[i], pFile);
 
     if (writeFile == EOF) {
       (void)printf("Write failed!\n");
       return 4;
-    } 
+    }
   }
   (void)printf("Write success!\n");
+
+  if (strcmp(openMode, "r") != 0) {
+    openMode = "r";
+    pFile = freopen(fileName, openMode, pFile);
+    if (pFile == NULL) {
+      (void)printf("Write Failed\n");
+      return 5;
+    }
+  }
+  (void)fRead(pFile);
 
   (void)printf("Closing file...\n");
   int closeStatus = fclose(pFile);
   if (closeStatus == EOF) {
     (void)printf("Read failed!\n");
-    return 5;
+    return 6;
   }
+
   return 0;
+}
+
+void fRead(FILE *pFile) {
+
+  char buffer[2][20];
+  int c = 0;
+  size_t j = 0;
+
+  for (size_t i = 0; i < sizeof(buffer)/ sizeof(buffer[0]); i++) {
+    j = 0;
+    while ((c = fgetc(pFile)) != '\n'){
+      buffer[i][j] = c;
+      j++;
+    }
+    buffer[i][j] = '\0';
+  }
+
+  for (size_t i = 0; i < sizeof(buffer)/ sizeof(buffer[0]); i++) {
+  (void)printf("%s\n", buffer[i]);
+  }
 }
